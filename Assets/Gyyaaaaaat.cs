cdb8471bc16afd9ee5443548b1f43e747471f6ef
Naval-Plug <myers.joseph05@gmail.com>
2026-03-31 15:05:04 -0500

    Spinny

--- Assets/Gyyaaaaaat.cs
+++ Assets/Gyyaaaaaat.cs
@@ -2,13 +2,32 @@ using Unity.VisualScripting;
 using UnityEngine;
 using Unity.Collections;
 
+
 public class Gyyaaaaaat : MonoBehaviour
 {
+    public bool changeColor = false;
+    public bool coolDown = true;
     private void OnCollisionEnter(Collision other)
     {
+      changeColor = true;
+    }
+    private void Update()
+    {
+        if (changeColor)
+        {
+            if(coolDown == true)
+            {
+             Invoke("Change", 0.1f);
+                coolDown = false;
+            }
+            
+        }
+        
+    }
+    void Change()
+    {
         GetComponent<MeshRenderer>().material.color = Random.ColorHSV();
-        bool hit = true;
+        coolDown = true;
     }
 
-  
 }
